4fc0b9370cea7703094c506a20c8066aeaf48c82
hankliu <397694072@qq.com>
2024-05-18 08:21:41 +0800

    feat(image-watermark): add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const PageTitle = 'H.L Toolkits - 图片水印';
+
+export const PageSubTitle = `在这个瞬息万变的数字时代，我们的在线图片水印工具，如同一位无声的艺术守护者，为您的每张照片增添独特的印记。操作简便，风格多样，无论您身在何处，轻松在线水印，让每一张图像都带着您的独特故事，成为永恒的艺术品。`;
+
+export const PageDescription = `在线图片水印网站，为您的每张照片增添独特的艺术印记，让您的创作在数字世界中熠熠生辉。`;
+
+export const PageKeywords = [
+  'image watermark',
+  '图片水印',
+  'toolkit',
+  'toolkits',
+  '前端开发',
+  '前端开发工具',
+  '前端开发工具集合',
+  '在线工具',
+  'toolbox',
+  'frontend',
+  '卡鲁秋',
+  'Hank',
+  'HankLiu',
+];
